7da6471e33c57ee5aabf269b0a009168e9348453
RickyJuvianto <ricky.juvianto98@gmail.com>
2017-08-31 19:15:51 +0700

    Passing Data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,10 +13,10 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
         //	
-		return "Hello... berhasil coy";
+		return "Hello... berhasil coy, ini post no. " . $id;
     }
 
     /**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,4 +15,4 @@
 //   return view('welcome');
 //});
 
-Route::get('/post', 'PostController@index');
+Route::get('/post/{id}', 'PostController@index');
