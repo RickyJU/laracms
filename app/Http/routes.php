dd0951dab775a6a51007648bc2b082b3ee3c5715
RickyJuvianto <ricky.juvianto98@gmail.com>
2017-08-31 20:22:20 +0700

    Passing Data to Views (cara 2)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,4 +21,4 @@ Route::resource('posts', 'PostController');
 
 Route::get('/contact', 'PostController@contact');
 
-Route::get('post/{id}', 'PostController@show_post');
+Route::get('post/{id}/{name}/{password}', 'PostController@show_post');
