99806701ae7b7d4c24a9bd31419cc9dae832d1c6
RickyJuvianto <ricky.juvianto98@gmail.com>
2017-09-01 14:57:13 +0700

    Master Layout Setup

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Laravel</title>
-	</head>
-	<body>
-		<div class="container">
-			<h1>Contact Page</h1>
-		</div>
-	</body>
-</html>
+@extends('layouts.app')
+
+
+@section('content')
+	<h1>Contact page</h1>
+    
+@stop
+
+@section('footer')
+	<script>alert("Hello Visitor.")</script>
+@stop
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--(!DOCTYPE html>
 <html>
 	<head>
 		<title>Laravel</title>
@@ -6,6 +6,21 @@
 	<body>
 		<div class="container">
 			<h1>Post {{$id}} {{$name}} {{$password}}</h1>
+@extends('layouts.app')
+
+@section('content')
+	<h1>Contact page</h1>
+
+@stop
 		</div>
 	</body>
 </html>
+-->
+
+@extends('layouts.app')
+
+
+@section('content')
+	<h1>Post {{$id}} {{$name}} {{$password}}</h1>
+
+@stop
