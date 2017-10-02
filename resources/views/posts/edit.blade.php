c2067ced061c0d6044b7f3dabc073e811cce5db3
RickyJuvianto <ricky.juvianto98@gmail.com>
2017-10-02 21:57:31 +0700

    Modifying Edit & Delete Forms with the Form Package

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,18 +5,32 @@
 
 	<h1>Edit Post</h1>
 	
-	<form action="/posts/{{$post->id}}" method="post">
+	<!-- <form action="/posts/{{$post->id}}" method="post">
 		<input type="hidden" name="_method" value="PUT" />
 		<input type="text" name="title" value="{{$post->title}}" />
 		{{csrf_field()}}
 		<input type="submit" name="submit" value="CREATE" />
-	</form>
+	</form> -->
 	
-	<form action="/posts/{{$post->id}}" method ="posts">
+	{!! Form::model($post, ['method'=>'PATCH', 'action'=>['PostController@update', $post->id]]) !!}
+		
+		{!! Form::label('title', 'Title:') !!}
+		{!! Form::text('title', null, ['class'=>'form-control']) !!}
+		
+		{!! Form::submit('Updated Post', ['class'=>'btn btn-info']) !!}
+	{!! Form::close() !!}
+	
+	<!-- <form action="/posts/{{$post->id}}" method ="posts">
 		<input type="hidden" name="_method" value="DELETE" />
 		{{csrf_field()}}
 		<input type="submit" value="DELETE" />
-	</form>
+	</form> -->
+	
+	{!! Form::open(['method'=>'DELETE', 'action'=>['PostController@destroy', $post->id]]) !!}
+		
+		{!! Form::submit('Delete Post', ['class'=>'btn btn-danger']) !!}
+		
+	{!! Form::close() !!}
 
 @stop
 
